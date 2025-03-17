2e59af80e650458b3fabb3b201faa1b1b5b29045
quanle1 <quanle1@beeketing.net>
2025-03-17 20:53:49 +0700

    Add Javascript demo

---
+++ Javascript_demo.js
@@ -0,0 +1,26 @@
+let a;
+let b;
+
+// Normal function
+function tong(a, b) {
+  return a + b;
+}
+
+console.log(tong(2, 7));
+
+// Lambda function (có thể không có "arguments")
+
+var message = () => console.log("Tích 2 số bằng:");
+message();
+var tich = (a, b) => {
+  return a + b;
+};
+console.log(tich(1, 5));
+
+// Anonymous function
+
+var tong2So = function (a, b) {
+  console.log("Tổng 2 số bằng:");
+  console.log(a + b);
+};
+tong2So(3, 8);
